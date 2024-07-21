08f3884ec9db2f62bdc05845f6a9c950f9beb12b
Morteza-Jan Azizi <mori.azizi@gmail.com>
2024-07-21 22:42:52 +0200

    some renaming done

--- deploy/terraform/provider.tf
+++ deploy/terraform/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 
   backend "s3" {
-    bucket = "my-terraform-state-bucket"
+    bucket = "aws-cloud-practitioner-terraform-state-bucket"
     key    = "terraform/state"
     region = var.aws_region
   }
