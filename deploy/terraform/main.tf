359762ebba0948e70bbfac39e882d7c8d72fa79c
Morteza <mori.azizi@gmail.com>
2024-08-02 20:20:39 +0200

    more changes

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,50 +1,21 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "13.2"
-  instance_class       = "db.t3.micro"
-  name                 = var.db_name
-  username             = var.db_username
-  password             = var.db_password
-  parameter_group_name = "default.postgres13"
-  db_subnet_group_name = aws_db_subnet_group.default.name
-  vpc_security_group_ids = [aws_security_group.default.id]
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
 
-  tags = {
-    Name = "aws-cloud-practitioner-postgres-db"
+  required_version = ">=0.14.9"
+
+    backend "s3" {
+    bucket = "aws-cloud-practitioner-terraform-state-bucket"
+    key    = "terraform/state"
+    region = "eu-west-1"
   }
 }
 
-resource "aws_db_subnet_group" "default" {
-  name       = "aws-cloud-practitioner-db-subnet-group"
-  subnet_ids = aws_subnet.default.*.id
-
-  tags = {
-    Name = "aws-cloud-practitioner-db-subnet-group"
-  }
-}
-
-resource "aws_security_group" "default" {
-  name        = "aws-cloud-practitioner-security-group"
-  description = "Allow all inbound traffic"
-  vpc_id      = aws_vpc.default.id
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "aws-cloud-practitioner-security-group"
-  }
+provider "aws" {
+  version = "~>3.0"
+  region  = "east-us-1"
 }
