4b222d078e5c3812ed3724e3615a553c71dcca32
Morteza Azizi <mori.azizi@gmail.com>
2024-08-03 23:37:46 +0200

    Adding action with basic terraform scripts (#2)
    * Adding action + terraform scripts
    
    * some renaming done
    
    * hard coding region backend in provider
    
    * hardcoding the region
    
    * more changes
    
    * version edited
    
    * moving all provider logics in provider file
    
    * fixing version issue
    
    * version value updated
    
    * upgrade to v3
    
    * some refactoring done
    
    * fixing errors
    
    * adding S3 and state
    
    * terraform fmt
    
    * fixing version issue
    
    * refactoring
    
    * disable the version
    
    * correcting the path
    
    * fixing provider issue
    
    * provider issue
    
    * try more changes
    
    * still trying
    
    * still trying to fix the provider format
    
    * action workflow updated
    
    * Update deploy.yml
    
    * Update deploy.yml

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "aws-cloud-practitioner-terraform-state-bucket"
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_db_instance" "postgres" {
+  allocated_storage   = 20
+  storage_type        = "gp2"
+  engine              = "postgres"
+  engine_version      = "16.2"
+  instance_class      = "db.t3.micro"
+  db_name             = "awscloudpractitionerdb"
+  username            = "awscloudpractitioner"
+  password            = "Password,1"
+  skip_final_snapshot = true
+
+  tags = {
+    Name = "PostgreSQL DB"
+  }
+}
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+    bucket  = "aws-cloud-practitioner-terraform-state-bucket"
+    key     = "terraform/state"
+    region  = "eu-west-1"
+    profile = ""
+    encrypt = "true"
+  }
+}
+
+
---
+++ deploy/terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "app_name" {
+  description = "Elastic Beanstalk application name"
+  type        = string
+}
+
+variable "db_name" {
+  description = "RDS database name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "RDS database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "RDS database password"
+  type        = string
+}
+
+variable "aws_db_instance" {
+  description = "RDS database Instance"
+  type        = string
+}
+
