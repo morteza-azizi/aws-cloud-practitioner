54a30e5d4a3cc52e51541113a76cfbe2d6438a8b
Morteza-Jan Azizi <mori.azizi@gmail.com>
2024-08-03 12:23:01 +0200

    adding S3 and state

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "terraform_state_bucket" {
   bucket = "aws-cloud-practitioner-terraform-state-bucket"
-  acl    = "public-read-write"
+  # acl    = "public-read-write"
 
   tags = {
     Name        = "Terraform State Bucket"
@@ -12,12 +12,11 @@ resource "aws_db_instance" "postgres" {
   allocated_storage    = 20                      
   storage_type         = "gp2"                   
   engine               = "postgres"              
-  engine_version       = "13.3"                  
+  engine_version       = "16.2"                  
   instance_class       = "db.t3.micro"           
-  db_name              = "aws-cloud-practitioner-db"
-  username             = "admin"                 
-  password             = "Password,1"          
-  parameter_group_name = "default.postgres13"    
+  db_name              = "awscloudpractitionerdb"
+  username             = "awscloudpractitioner"                 
+  password             = "Password,1"
   skip_final_snapshot  = true                    
 
   tags = {
@@ -25,13 +24,14 @@ resource "aws_db_instance" "postgres" {
   }
 }
 
-# terraform {
-#   required_version = ">= 1.0.0"
+terraform {
+  required_version = ">= 1.0.0"
 
-#   backend "s3" {
-#     bucket = "aws-cloud-practitioner-terraform-state-bucket"
-#     key    = "terraform/state"
-#     region = "eu-west-1"
-#   }
-# }
+  backend "s3" {
+    bucket = "aws-cloud-practitioner-terraform-state-bucket"
+    key    = "terraform/state"
+    region = "eu-west-1"
+  }
+}
+
 
